e7fb3d50cd16edf058f2c9c8ca001b81745e5442
ArtemShapoval <167710600+ArtemShapoval@users.noreply.github.com>
2025-02-11 10:39:01 +0200

    Update main.cpp

--- c_plusplus_part2/lb1/main.cpp
+++ c_plusplus_part2/lb1/main.cpp
@@ -17,4 +17,24 @@ int main(void) {
 
     cout << "Найменший елемент: " << minValue << endl;
     return 0;
+    /* const int size = 10;
+    int matrix[size][size] = {0};
+    
+    int *ptr = &matrix[0][0]; // Вказівник на перший елемент масиву
+    
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            *(ptr + i * size + j) = (i + 1) * (j + 1); // Заповнення таблиці множення
+        }
+    }
+    
+    // Виведення таблиці множення
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            std::cout << *(ptr + i * size + j) << "\t";
+        }
+        std::cout << std::endl;
+    }
+    
+    return 0;*/
 }
